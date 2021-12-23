6eb8f5d16aeb5205b61bb338e7ce3e591aa3f95c
krsusmit <95850079+krsusmit@users.noreply.github.com>
2021-12-23 21:50:17 +0530

    Add files via upload

---
+++ core-java-assignments/Assignment7/src/question2/Custom.java
@@ -0,0 +1,23 @@
+package question2;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@interface info{
+    int authorID();
+    String author() default "Null";
+    String supervisor() default "Null";
+    String date();
+    String time();
+    int version();
+    String description() default "General class";
+}
+
+
+@info(authorID = 12, date = "21 March", time = "4pm", version = 6)
+public class Custom {
+    public static void main(String[] args) {
+
+    }
+}
