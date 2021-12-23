dbe903488a122344041760396e4fba6102971028
krsusmit <95850079+krsusmit@users.noreply.github.com>
2021-12-23 20:56:48 +0530

    Add files via upload

---
+++ joining.java
@@ -0,0 +1,10 @@
+public class joining
+{
+    public static void main(String[] args) {
+    	
+        String str1 = "Hello";
+        String str2 = str1.concat(" How are you?");
+        
+        System.out.println("The cancated Strings are : " + str2);
+    }
+}
